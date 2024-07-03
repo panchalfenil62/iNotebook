876cf799c82d9f7f7c85700df5d9061782b84866
Fenil Panchal <panchalfenil620@gmail.com>
2024-07-04 00:14:52 +0530

    changed

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -8,7 +8,7 @@ const fetchuser = require('../middleware/fetchUser');
 router.get('/fetchallnotes', fetchuser, async (req,res) => {
     try{
     const notes = await Note.find({user: req.user.id});
-    if(notes){return res.json(notes);}
+    res.json(notes);
     }catch (error) {
         console.error(error.message);
         res.status(500).send("Internal Server Error.")
