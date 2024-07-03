827a6c8d5c341781014269dd04464cdd67a9c5a7
Fenil Panchal <panchalfenil620@gmail.com>
2024-07-03 21:46:40 +0530

    changes

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/inotebook';
+const mongoURI = 'mongodb://0.0.0.0:27017/inotebook';
 
 const connectToMongo = () =>{
     mongoose.connect(mongoURI).then(()=>console.log("Connected")).catch((e)=>console.log(e.message))
